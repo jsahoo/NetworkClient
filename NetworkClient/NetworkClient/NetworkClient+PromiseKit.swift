86ddbea0f41af5f503b5ba825d2230a59bae16a7
Jonathan Sahoo <sahoojonathan@gmail.com>
2020-07-03 10:16:40 -0700

    WIP - Major Refactor

--- NetworkClient/NetworkClient/NetworkClient+PromiseKit.swift
+++ NetworkClient/NetworkClient/NetworkClient+PromiseKit.swift
@@ -11,18 +11,30 @@
 import Foundation
 import PromiseKit
 
-extension NetworkClient {
+extension NetworkRequest {
 
     /// Executes a network request that returns Data.
-    public static func requestDataWithMetadata(url: String,
-                                               method: HTTPMethod = .get,
-                                               queryParameters: Parameters? = nil,
-                                               body: HTTPBody? = nil,
-                                               headers: [String: String]? = nil,
-                                               validStatusCodes: [Int] = HTTPStatusCodes.successes) -> Promise<(Data, ResponseMetadata?)> {
-
+    func responseData() -> Promise<Data> {
         return Promise { seal in
-            requestData(url: url, method: method, queryParameters: queryParameters, body: body, headers: headers, validStatusCodes: validStatusCodes) { result, metadata in
+            responseData { result, metadata in
+                switch result {
+                case .success(let data):
+                    return seal.fulfill(data)
+                case .failure(let error):
+                    return seal.reject(error)
+                }
+            }
+        }
+
+//        return responseDataWithMetadata().then { data, _ -> Promise<Data> in
+//            return .value(data)
+//        }
+    }
+
+    /// Executes a network request that returns Data.
+    func responseDataWithMetadata() -> Promise<(Data, ResponseMetadata?)> {
+        return Promise { seal in
+            responseData { result, metadata in
                 switch result {
                 case .success(let data):
                     return seal.fulfill((data, metadata))
@@ -33,62 +45,10 @@ extension NetworkClient {
         }
     }
 
-    /// Executes a network request that returns Data.
-    public static func requestData(url: String,
-                                   method: HTTPMethod = .get,
-                                   queryParameters: Parameters? = nil,
-                                   body: HTTPBody? = nil,
-                                   headers: [String: String]? = nil,
-                                   validStatusCodes: [Int] = HTTPStatusCodes.successes) -> Promise<Data> {
-
-        // TWO OPTIONS - HANDLE WITH PROMISES, OR HANDLE WITH CLOSURES
-        return requestDataWithMetadata(url: url, method: method, queryParameters: queryParameters, body: body, headers: headers, validStatusCodes: validStatusCodes).then { date, metadata -> Promise<Data> in
-            return .value(date)
-        }
-
-        return Promise { seal in
-            requestData(url: url, method: method, queryParameters: queryParameters, body: body, headers: headers, validStatusCodes: validStatusCodes) { result, _ in
-                switch result {
-                case .success(let data):
-                    return seal.fulfill(data)
-                case .failure(let error):
-                    return seal.reject(error)
-                }
-            }
-        }
-    }
-
-    /// Executes a network request that returns Data.
-    public static func requestData(baseURL: String? = baseURL?(),
-                                   path: String,
-                                   method: HTTPMethod = .get,
-                                   queryParameters: Parameters? = nil,
-                                   body: HTTPBody? = nil,
-                                   headers: [String: String]? = nil,
-                                   validStatusCodes: [Int] = HTTPStatusCodes.successes) -> Promise<Data> {
-
-        return Promise { seal in
-            requestData(baseURL: baseURL, path: path, method: method, queryParameters: queryParameters, body: body, headers: headers, validStatusCodes: validStatusCodes) { result in
-                switch result {
-                case .success(let data):
-                    return seal.fulfill(data)
-                case .failure(let error):
-                    return seal.reject(error)
-                }
-            }
-        }
-    }
-
     /// Executes a network request that returns Void.
-    public static func requestVoid(url: String,
-                                   method: HTTPMethod = .get,
-                                   queryParameters: Parameters? = nil,
-                                   body: HTTPBody? = nil,
-                                   headers: [String: String]? = nil,
-                                   validStatusCodes: [Int] = HTTPStatusCodes.successes) -> Promise<Void> {
-
+    func requestVoid() -> Promise<Void> {
         return Promise { seal in
-            requestVoid(url: url, method: method, queryParameters: queryParameters, body: body, headers: headers, validStatusCodes: validStatusCodes) { result in
+            responseVoid { result, metadata in
                 switch result {
                 case .success:
                     return seal.fulfill(())
@@ -100,16 +60,23 @@ extension NetworkClient {
     }
 
     /// Executes a network request that returns Void.
-    public static func requestVoid(baseURL: String? = baseURL?(),
-                                   path: String,
-                                   method: HTTPMethod = .get,
-                                   queryParameters: Parameters? = nil,
-                                   body: HTTPBody? = nil,
-                                   headers: [String: String]? = nil,
-                                   validStatusCodes: [Int] = HTTPStatusCodes.successes) -> Promise<Void> {
-
+    func requestVoidWithMetadata() -> Promise<(Void, ResponseMetadata?)> {
         return Promise { seal in
-            requestVoid(baseURL: baseURL, path: path, method: method, queryParameters: queryParameters, body: body, headers: headers, validStatusCodes: validStatusCodes) { result in
+            responseVoid { result, metadata in
+                switch result {
+                case .success:
+                    return seal.fulfill(((), metadata))
+                case .failure(let error):
+                    return seal.reject(error)
+                }
+            }
+        }
+    }
+
+    /// Executes a network request that returns Void.
+    func requestJSON() -> Promise<Void> {
+        return Promise { seal in
+            responseVoid { result, metadata in
                 switch result {
                 case .success:
                     return seal.fulfill(())
@@ -120,80 +87,13 @@ extension NetworkClient {
         }
     }
 
-    /// Executes a network request that returns JSON.
-    public static func requestJSON(url: String,
-                                   method: HTTPMethod = .get,
-                                   queryParameters: Parameters? = nil,
-                                   body: HTTPBody? = nil,
-                                   headers: [String: String]? = nil,
-                                   validStatusCodes: [Int] = HTTPStatusCodes.successes) -> Promise<Any> {
-
+    /// Executes a network request that returns Void.
+    func requestVoidWithMetadata() -> Promise<(Void, ResponseMetadata?)> {
         return Promise { seal in
-            requestJSON(url: url, method: method, queryParameters: queryParameters, body: body, headers: headers, validStatusCodes: validStatusCodes) { result in
+            responseVoid { result, metadata in
                 switch result {
-                case .success(let data):
-                    return seal.fulfill(data)
-                case .failure(let error):
-                    return seal.reject(error)
-                }
-            }
-        }
-    }
-
-    /// Executes a network request that returns JSON.
-    public static func requestJSON(baseURL: String? = baseURL?(),
-                                   path: String,
-                                   method: HTTPMethod = .get,
-                                   queryParameters: Parameters? = nil,
-                                   body: HTTPBody? = nil,
-                                   headers: [String: String]? = nil,
-                                   validStatusCodes: [Int] = HTTPStatusCodes.successes) -> Promise<Any> {
-
-        return Promise { seal in
-            requestJSON(baseURL: baseURL, path: path, method: method, queryParameters: queryParameters, body: body, headers: headers, validStatusCodes: validStatusCodes) { result in
-                switch result {
-                case .success(let data):
-                    return seal.fulfill(data)
-                case .failure(let error):
-                    return seal.reject(error)
-                }
-            }
-        }
-    }
-
-    /// Executes a network request that returns a Decodable object.
-    public static func request<T: Decodable>(url: String,
-                                             method: HTTPMethod = .get,
-                                             queryParameters: Parameters? = nil,
-                                             body: HTTPBody? = nil,
-                                             headers: [String: String]? = nil,
-                                             validStatusCodes: [Int] = HTTPStatusCodes.successes) -> Promise<T> {
-
-        return Promise { seal in
-            request(url: url, method: method, queryParameters: queryParameters, body: body, headers: headers, validStatusCodes: validStatusCodes) { (result: Swift.Result<T, Error>) in
-                switch result {
-                case .success(let data):
-                    return seal.fulfill(data)
-                case .failure(let error):
-                    return seal.reject(error)
-                }
-            }
-        }
-    }
-
-    public static func request<T: Decodable>(baseURL: String? = baseURL?(),
-                                             path: String,
-                                             method: HTTPMethod = .get,
-                                             queryParameters: Parameters? = nil,
-                                             body: HTTPBody? = nil,
-                                             headers: [String: String]? = nil,
-                                             validStatusCodes: [Int] = HTTPStatusCodes.successes) -> Promise<T> {
-
-        return Promise { seal in
-            request(baseURL: baseURL, path: path, method: method, queryParameters: queryParameters, body: body, headers: headers, validStatusCodes: validStatusCodes) { (result: Swift.Result<T, Error>) in
-                switch result {
-                case .success(let data):
-                    return seal.fulfill(data)
+                case .success:
+                    return seal.fulfill(((), metadata))
                 case .failure(let error):
                     return seal.reject(error)
                 }
--- NetworkClient/NetworkClient/NetworkClient.swift
+++ NetworkClient/NetworkClient/NetworkClient.swift
@@ -99,61 +99,88 @@ public struct ResponseMetadata {
 
 public class NetworkClient {
 
-    /// If many of your APIs share the same base URL, you can set the default base URL here through this closure and use the NetworkClient request functions that construct the URL from this base URL and the supplied path.
-    ///
-    /// We use a closure so that the base URL can be determined at execution time, i.e. to support dynamic base URLs that change based on the current environment.
-    public static var baseURL: (() -> String)?
-    
     private static let networkMonitor = NWPathMonitor()
     public private(set) static var hasNetworkConnection = true
-    private static var hasBeenInitialized = false
-    
-    private static let session = URLSession(configuration: .default)
+    fileprivate static var hasBeenInitialized = false
+
+    fileprivate static let session = URLSession(configuration: .default)
     
     public static func initialize() {
+        guard !hasBeenInitialized else { return }
         networkMonitor.pathUpdateHandler = { path in
             hasNetworkConnection = path.status == .satisfied
         }
         networkMonitor.start(queue: DispatchQueue(label: "NWPathMonitor"))
         hasBeenInitialized = true
     }
+}
 
+public class NetworkRequest {
 
-    /// Executes a network request that returns Data.
+    public class var baseURL: String {
+        return ""
+    }
+
+    public var url: String
+    public var method: HTTPMethod = .get
+    public var queryParameters: Parameters? = nil
+    public var body: HTTPBody? = nil
+    public var headers: [String: String]? = nil
+    public var validStatusCodes: [Int] = HTTPStatusCodes.successes
+
+    /// Initialize a NetworkRequest for the given URL.
+    public init(url: String,
+                method: HTTPMethod = .get,
+                queryParameters: Parameters? = nil,
+                body: HTTPBody? = nil,
+                headers: [String: String]? = nil,
+                validStatusCodes: [Int] = HTTPStatusCodes.successes) {
+        self.url = url
+        self.method = method
+        self.queryParameters = queryParameters
+        self.body = body
+        self.headers = headers
+        self.validStatusCodes = validStatusCodes
+    }
+
+    /// Initialize a NetworkRequest using the default base URL and the given path.
+    public convenience init(path: String,
+                            method: HTTPMethod = .get,
+                            queryParameters: Parameters? = nil,
+                            body: HTTPBody? = nil,
+                            headers: [String: String]? = nil,
+                            validStatusCodes: [Int] = HTTPStatusCodes.successes) {
+        self.init(url: NSString.path(withComponents: [NetworkRequest.baseURL, path]), method: method, queryParameters: queryParameters, body: body, headers: headers, validStatusCodes: validStatusCodes)
+    }
+
+    /// Executes a network request and returns the raw data from the response.
     ///
     /// This is where the actual network request for all NetworkClient request functions is performed. All other NetworkClient request functions utilize this function for making the actual network request.
-    public static func requestData(url urlString: String,
-                                   method: HTTPMethod = .get,
-                                   queryParameters: Parameters? = nil,
-                                   body: HTTPBody? = nil,
-                                   headers: [String: String]? = nil,
-                                   validStatusCodes: [Int] = HTTPStatusCodes.successes,
-                                   completion: @escaping ((Result<Data, Error>, ResponseMetadata?) -> Void)) {
-        
-        guard hasBeenInitialized else {
-            fatalError("NetworkClient has not been initialized. Call NetworkClient.initialize() before performing any other functions.")
-        }
+    public func responseData(completion: @escaping ((Swift.Result<Data, Error>, ResponseMetadata?) -> Void)) {
+
+        NetworkClient.initialize()
 
         // MARK: Build Request
 
-        guard var urlComponents = URLComponents(string: urlString) else {
-            completion(.failure(NetworkError.invalidURL(url: urlString)), nil)
+        guard var urlComponents = URLComponents(string: url) else {
+            completion(.failure(NetworkError.invalidURL(url: url)), nil)
             return
         }
-        
+
         if let queryParameters = queryParameters {
             urlComponents.queryItems = queryParameters.map( { URLQueryItem(name: $0.0, value: "\($0.1)") } )
         }
-        
+
+        let urlString = url
         guard let url = urlComponents.url else {
             completion(.failure(NetworkError.invalidURL(url: urlString)), nil)
             return
         }
-        
+
         var urlRequest = URLRequest(url: url)
         urlRequest.httpMethod = method.rawValue
         var metadata = ResponseMetadata(request: urlRequest)
-        
+
         if let body = body as? HTTPBodyFromDictionary {
             urlRequest.addValue(body.format.headerValue, forHTTPHeaderField: "Content-Type")
             switch body.format {
@@ -171,18 +198,18 @@ public class NetworkClient {
             urlRequest.addValue("application/json", forHTTPHeaderField: "Content-Type")
             urlRequest.httpBody = body.data
         }
-        
+
         headers?.forEach { urlRequest.addValue($0.value, forHTTPHeaderField: $0.key) }
         metadata.request = urlRequest
 
         // MARK: Execute Request
 
-        guard hasNetworkConnection else {
+        guard NetworkClient.hasNetworkConnection else {
             completion(.failure(NetworkError.noNetworkConnection), metadata)
             return
         }
-        
-        session.dataTask(with: urlRequest) { (data, urlResponse, error) in
+
+        NetworkClient.session.dataTask(with: urlRequest) { (data, urlResponse, error) in
             metadata.data = data
             metadata.response = urlResponse as? HTTPURLResponse
 
@@ -190,188 +217,77 @@ public class NetworkClient {
                 completion(.failure(error), metadata)
                 return
             }
-            
+
             guard let urlResponse = urlResponse as? HTTPURLResponse else {
                 completion(.failure(NetworkError.noResponse), metadata)
                 return
             }
-                
-            guard validStatusCodes.contains(urlResponse.statusCode) else {
+
+            guard self.validStatusCodes.contains(urlResponse.statusCode) else {
                 completion(.failure(NetworkError.invalidStatusCode(urlResponse.statusCode)), metadata)
                 return
             }
-            
+
             guard let unwrappedData = data else {
                 completion(.failure(NetworkError.noData), metadata)
                 return
             }
-            
+
             completion(.success(unwrappedData), metadata)
         }.resume()
     }
 
-    /// Executes a network request that returns Data.
-    public static func requestData(baseURL: String? = baseURL?(),
-                                   path: String,
-                                   method: HTTPMethod = .get,
-                                   queryParameters: Parameters? = nil,
-                                   body: HTTPBody? = nil,
-                                   headers: [String: String]? = nil,
-                                   validStatusCodes: [Int] = HTTPStatusCodes.successes,
-                                   completion: @escaping ((Result<Data, Error>, ResponseMetadata?) -> Void)) {
+    /// Executes a network request and returns Void.
+    public func responseVoid(completion: @escaping ((Result<Void, Error>, ResponseMetadata?) -> Void)) {
 
-        guard let baseURL = baseURL else {
-            completion(.failure(NetworkError.missingBaseURL), nil)
-            return
-        }
-
-        let urlString = NSString.path(withComponents: [baseURL, path])
-
-        requestData(url: urlString, method: method, queryParameters: queryParameters, body: body, headers: headers, validStatusCodes: validStatusCodes, completion: completion)
-    }
-
-    /// Executes a network request that returns Void.
-    public static func requestVoid(url: String,
-                                   method: HTTPMethod = .get,
-                                   queryParameters: Parameters? = nil,
-                                   body: HTTPBody? = nil,
-                                   headers: [String: String]? = nil,
-                                   validStatusCodes: [Int] = HTTPStatusCodes.successes,
-                                   completion: @escaping ((ResponseMetadata<Void>) -> Void)) {
-
-        requestData(url: url, method: method, queryParameters: queryParameters, body: body, headers: headers, validStatusCodes: validStatusCodes) { response in
+        responseData { result, metadata in
 
             // `result` will _never_ be `success`. We have to examine the error to determine if the request was successful or not
 
-            switch response.result {
+            switch result {
             case .success:
-                completion(transformSuccessfulResponse(response, newValue: ()))
+                completion(.success(()), metadata)
             case .failure(let error):
                 if case NetworkError.noData = error {
-                    completion(transformSuccessfulResponse(response, newValue: ()))
+                    completion(.success(()), metadata)
                 } else {
-                    completion(transformErrorResponse(response, error: error))
+                    completion(.failure(error), metadata)
                 }
             }
         }
     }
 
-    /// Executes a network request that returns Void.
-    public static func requestVoid(baseURL: String? = baseURL?(),
-                                   path: String,
-                                   method: HTTPMethod = .get,
-                                   queryParameters: Parameters? = nil,
-                                   body: HTTPBody? = nil,
-                                   headers: [String: String]? = nil,
-                                   validStatusCodes: [Int] = HTTPStatusCodes.successes,
-                                   completion: @escaping ((Result<Void, Error>) -> Void)) {
+    /// Executes a network request and deserializes the response to JSON.
+    public func responseJSON(completion: @escaping ((Result<Any, Error>, ResponseMetadata?) -> Void)) {
 
-        guard let baseURL = baseURL else {
-            completion(.failure(NetworkError.missingBaseURL))
-            return
-        }
-
-        let url = NSString.path(withComponents: [baseURL, path])
-
-        requestVoid(url: url, method: method, queryParameters: queryParameters, body: body, headers: headers, validStatusCodes: validStatusCodes, completion: completion)
-    }
-
-    /// Executes a network request that returns JSON.
-    public static func requestJSON(url: String,
-                                   method: HTTPMethod = .get,
-                                   queryParameters: Parameters? = nil,
-                                   body: HTTPBody? = nil,
-                                   headers: [String: String]? = nil,
-                                   validStatusCodes: [Int] = HTTPStatusCodes.successes,
-                                   completion: @escaping ((Result<Any, Error>) -> Void)) {
-
-        requestData(url: url, method: method, queryParameters: queryParameters, body: body, headers: headers, validStatusCodes: validStatusCodes) { result in
-
+        responseData { result, metadata in
             switch result {
             case .success(let data):
                 do {
-                    completion(.success(try JSONSerialization.jsonObject(with: data)))
+                    completion(.success(try JSONSerialization.jsonObject(with: data)), metadata)
                 } catch {
-                    completion(.failure(error))
+                    completion(.failure(error), metadata)
                 }
             case .failure(let error):
-                completion(.failure(error))
+                completion(.failure(error), metadata)
             }
         }
     }
 
-    /// Executes a network request that returns JSON.
-    public static func requestJSON(baseURL: String? = baseURL?(),
-                                   path: String,
-                                   method: HTTPMethod = .get,
-                                   queryParameters: Parameters? = nil,
-                                   body: HTTPBody? = nil,
-                                   headers: [String: String]? = nil,
-                                   validStatusCodes: [Int] = HTTPStatusCodes.successes,
-                                   completion: @escaping ((Result<Any, Error>) -> Void)) {
+    /// Executes a network request and deserializes the response to a Decodable object.
+    public func responseDecodable<T: Decodable>(completion: @escaping ((Result<T, Error>, ResponseMetadata?) -> Void)) {
 
-        guard let baseURL = baseURL else {
-            completion(.failure(NetworkError.missingBaseURL))
-            return
-        }
-
-        let url = NSString.path(withComponents: [baseURL, path])
-
-        requestJSON(url: url, method: method, queryParameters: queryParameters, body: body, headers: headers, validStatusCodes: validStatusCodes, completion: completion)
-    }
-
-    /// Executes a network request that returns a Decodable object.
-    public static func request<T: Decodable>(url: String,
-                                             method: HTTPMethod = .get,
-                                             queryParameters: Parameters? = nil,
-                                             body: HTTPBody? = nil,
-                                             headers: [String: String]? = nil,
-                                             validStatusCodes: [Int] = HTTPStatusCodes.successes,
-                                             completion: @escaping ((Result<T, Error>) -> Void)) {
-
-        requestData(url: url, method: method, queryParameters: queryParameters, body: body, headers: headers, validStatusCodes: validStatusCodes) { result in
-
+        responseData { result, metadata in
             switch result {
             case .success(let data):
                 do {
-                    completion(.success(try JSONDecoder().decode(T.self, from: data)))
+                    completion(.success(try JSONDecoder().decode(T.self, from: data)), metadata)
                 } catch {
-                    completion(.failure(error))
+                    completion(.failure(error), metadata)
                 }
             case .failure(let error):
-                completion(.failure(error))
+                completion(.failure(error), metadata)
             }
         }
-    }
-
-    /// Executes a network request that returns a Decodable object.
-    public static func request<T: Decodable>(baseURL: String? = baseURL?(),
-                                             path: String,
-                                             method: HTTPMethod = .get,
-                                             queryParameters: Parameters? = nil,
-                                             body: HTTPBody? = nil,
-                                             headers: [String: String]? = nil,
-                                             validStatusCodes: [Int] = HTTPStatusCodes.successes,
-                                             completion: @escaping ((Result<T, Error>) -> Void)) {
-
-        guard let baseURL = baseURL else {
-            completion(.failure(NetworkError.missingBaseURL))
-            return
-        }
-
-        let url = NSString.path(withComponents: [baseURL, path])
-
-        request(url: url, method: method, queryParameters: queryParameters, body: body, headers: headers, validStatusCodes: validStatusCodes, completion: completion)
-    }
-}
-
-extension NetworkClient {
-
-    fileprivate static func transformSuccessfulResponse<T,U>(_ response: ResponseMetadata<T>, newValue: U) -> ResponseMetadata<U> {
-        return ResponseMetadata(value: newValue, request: response.request, response: response.response, data: response.data)
-    }
-
-    fileprivate static func transformErrorResponse<T,U>(_ response: ResponseMetadata<T>, error: Error) -> ResponseMetadata<U> {
-        return .init(error: error, request: response.request, response: response.response, data: response.data)
     }
 }
