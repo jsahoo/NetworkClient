5bf7dcd86c3e2757eabd745811d9428fa17a3064
Jonathan Sahoo <sahoojonathan@gmail.com>
2020-07-03 10:39:55 -0700

    Promises

--- NetworkClient/NetworkClient/NetworkClient+PromiseKit.swift
+++ NetworkClient/NetworkClient/NetworkClient+PromiseKit.swift
@@ -13,25 +13,17 @@ import PromiseKit
 
 extension NetworkRequest {
 
-    /// Executes a network request that returns Data.
+    /// Executes a network request and returns the raw data from the response.
     func responseData() -> Promise<Data> {
-        return Promise { seal in
-            responseData { result, metadata in
-                switch result {
-                case .success(let data):
-                    return seal.fulfill(data)
-                case .failure(let error):
-                    return seal.reject(error)
-                }
-            }
+        return responseDataWithMetadata().then { data, _ -> Promise<Data> in
+            return .value(data)
+        }.recover { error -> Promise<Data> in
+            // Re-throw the underlying error and exclude response metadata
+            throw (error as? PromiseNetworkError)?.error ?? error
         }
-
-//        return responseDataWithMetadata().then { data, _ -> Promise<Data> in
-//            return .value(data)
-//        }
     }
 
-    /// Executes a network request that returns Data.
+    /// Executes a network request and returns the raw data from the response.
     func responseDataWithMetadata() -> Promise<(Data, ResponseMetadata?)> {
         return Promise { seal in
             responseData { result, metadata in
@@ -39,67 +31,88 @@ extension NetworkRequest {
                 case .success(let data):
                     return seal.fulfill((data, metadata))
                 case .failure(let error):
-                    return seal.reject(error)
+                    return seal.reject(PromiseNetworkError(error: error, responseMetadata: metadata))
                 }
             }
         }
     }
 
-    /// Executes a network request that returns Void.
-    func requestVoid() -> Promise<Void> {
-        return Promise { seal in
-            responseVoid { result, metadata in
-                switch result {
-                case .success:
-                    return seal.fulfill(())
-                case .failure(let error):
-                    return seal.reject(error)
-                }
-            }
+    /// Executes a network request and returns Void.
+    func responseVoid() -> Promise<Void> {
+        return responseVoidWithMetadata().then { void, _ -> Promise<Void> in
+            return .value(void)
+        }.recover { error -> Promise<Void> in
+            // Re-throw the underlying error and exclude response metadata
+            throw (error as? PromiseNetworkError)?.error ?? error
         }
     }
 
-    /// Executes a network request that returns Void.
-    func requestVoidWithMetadata() -> Promise<(Void, ResponseMetadata?)> {
-        return Promise { seal in
-            responseVoid { result, metadata in
-                switch result {
-                case .success:
-                    return seal.fulfill(((), metadata))
-                case .failure(let error):
-                    return seal.reject(error)
-                }
-            }
-        }
-    }
-
-    /// Executes a network request that returns Void.
-    func requestJSON() -> Promise<Void> {
-        return Promise { seal in
-            responseVoid { result, metadata in
-                switch result {
-                case .success:
-                    return seal.fulfill(())
-                case .failure(let error):
-                    return seal.reject(error)
-                }
-            }
-        }
-    }
-
-    /// Executes a network request that returns Void.
-    func requestVoidWithMetadata() -> Promise<(Void, ResponseMetadata?)> {
+    /// Executes a network request and returns Void.
+    func responseVoidWithMetadata() -> Promise<(Void, ResponseMetadata?)> {
         return Promise { seal in
             responseVoid { result, metadata in
                 switch result {
                 case .success:
                     return seal.fulfill(((), metadata))
                 case .failure(let error):
-                    return seal.reject(error)
+                    return seal.reject(PromiseNetworkError(error: error, responseMetadata: metadata))
                 }
             }
         }
     }
+
+    /// Executes a network request and deserializes the response to JSON.
+    func responseJSON() -> Promise<Any> {
+        return responseJSONWithMetadata().then { json, _ -> Promise<Any> in
+            return .value(json)
+        }.recover { error -> Promise<Any> in
+            // Re-throw the underlying error and exclude response metadata
+            throw (error as? PromiseNetworkError)?.error ?? error
+        }
+    }
+
+    /// Executes a network request and deserializes the response to JSON.
+    func responseJSONWithMetadata() -> Promise<(Any, ResponseMetadata?)> {
+        return Promise { seal in
+            responseJSON { result, metadata in
+                switch result {
+                case .success(let json):
+                    return seal.fulfill((json, metadata))
+                case .failure(let error):
+                    return seal.reject(PromiseNetworkError(error: error, responseMetadata: metadata))
+                }
+            }
+        }
+    }
+
+    /// Executes a network request and deserializes the response to JSON.
+    func responseDecodable<T: Decodable>() -> Promise<T> {
+        return responseDecodableWithMetadata().then { object, _ -> Promise<T> in
+            return .value(object)
+        }.recover { error -> Promise<T> in
+            // Re-throw the underlying error and exclude response metadata
+            throw (error as? PromiseNetworkError)?.error ?? error
+        }
+    }
+
+    /// Executes a network request and deserializes the response to JSON.
+    func responseDecodableWithMetadata<T: Decodable>() -> Promise<(T, ResponseMetadata?)> {
+        return Promise { seal in
+            responseDecodable { (result: Swift.Result<T, Error>, metadata: ResponseMetadata?) in
+                switch result {
+                case .success(let object):
+                    return seal.fulfill((object, metadata))
+                case .failure(let error):
+                    return seal.reject(PromiseNetworkError(error: error, responseMetadata: metadata))
+                }
+            }
+        }
+    }
+}
+
+struct PromiseNetworkError: Error {
+    var error: Error
+    var responseMetadata: ResponseMetadata?
 }
 
 #endif
