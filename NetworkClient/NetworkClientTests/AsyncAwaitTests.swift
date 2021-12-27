0ff57f92a3ff2334b668b9826007a2fcac83e59e
Jonathan Sahoo <sahoojonathan@gmail.com>
2021-12-27 14:35:00 -0800

    Finalized async/await implementation

--- NetworkClient/NetworkClientTests/AsyncAwaitTests.swift
+++ NetworkClient/NetworkClientTests/AsyncAwaitTests.swift
@@ -20,105 +20,57 @@ class AsyncAwaitTests: XCTestCase {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
 
-    func testResponseDataWithMetadata() async throws {
-        let (_, metadata) = try await NetworkRequest(url: "https://postman-echo.com/get").responseDataWithMetadata()
-        XCTAssertNotNil(metadata)
-    }
-
     func testResponseData() async throws {
-        _ = try await NetworkRequest(url: "https://postman-echo.com/get").responseData()
-    }
-
-    func testResponseVoidWithMetadata() async throws {
-        let (_, metadata) = try await NetworkRequest(url: "https://postman-echo.com/get").responseVoidWithMetadata()
-        XCTAssertNotNil(metadata)
-    }
-
-    func testResponseVoid() async throws {
-        try await NetworkRequest(url: "https://postman-echo.com/get").responseVoid()
-    }
-
-    func testResponseJSONWithMetadata() async throws {
-        let (json, metadata) = try await NetworkRequest(url: "https://postman-echo.com/get").responseJSONWithMetadata()
-        XCTAssertNotNil(json)
-        XCTAssertNotNil(metadata)
-    }
-
-    func testResponseJSON() async throws {
-        let json = try await NetworkRequest(url: "https://postman-echo.com/get").responseJSON()
-        XCTAssertNotNil(json)
-    }
-
-    func testResponseDecodableWithMetadata() async throws {
-        let url = "https://postman-echo.com/get?foo1=bar1&foo2=bar2"
-        let (codableResponse, metadata) = try await NetworkRequest(url: url).responseDecodableWithMetadata() as (PostmanGetResponseCodable, ResponseMetadata?)
-        XCTAssertEqual(codableResponse.args, ["foo1": "bar1", "foo2": "bar2"])
-        XCTAssertNotNil(metadata)
-    }
-
-    func testResponseDecodable() async throws {
-        let url = "https://postman-echo.com/get?foo1=bar1&foo2=bar2"
-        let responseArgs = try await (NetworkRequest(url: url).responseDecodable() as PostmanGetResponseCodable).args
-        XCTAssertEqual(responseArgs, ["foo1": "bar1", "foo2": "bar2"])
-    }
-
-    // MARK: V2 Functions
-
-    func testResponseDataV2() async throws {
-        let response = try await NetworkRequest(url: "https://postman-echo.com/get").responseDataV2()
-        let data = response.object
-        XCTAssertNotNil(data)
-        XCTAssertNotNil(response.metadata)
-    }
-
-    func testResponseVoidV2() async throws {
-        let response = try await NetworkRequest(url: "https://postman-echo.com/get").responseVoidV2()
-        let void: Void = response.object
-        XCTAssertNotNil(void)
-        XCTAssertNotNil(response.metadata)
-    }
-
-    func testResponseJSONV2() async throws {
-        let response = try await NetworkRequest(url: "https://postman-echo.com/get").responseJSONV2()
-        let json = response.object
-        XCTAssertNotNil(json)
-        XCTAssertNotNil(response.metadata)
-    }
-
-    func testResponseDecodableV2() async throws {
-        let url = "https://postman-echo.com/get?foo1=bar1&foo2=bar2"
-        let response = try await NetworkRequest(url: url).responseDecodableV2() as NetworkResponse<PostmanGetResponseCodable>
-        let postmanGetResponseModel = response.object
-        XCTAssertEqual(postmanGetResponseModel.args, ["foo1": "bar1", "foo2": "bar2"])
-        XCTAssertNotNil(response.metadata)
-    }
-
-    // MARK: V3 Functions
-
-    func testResponseDataV3() async throws {
-        let response = try await NetworkRequest(url: "https://postman-echo.com/get").responseDataV3()
+        let response = try await NetworkRequest(url: "https://postman-echo.com/get").responseData()
         let data = response.data
         XCTAssertNotNil(data)
         XCTAssertNotNil(response.metadata)
     }
 
-    func testResponseVoidV3() async throws {
-        let metadata = try await NetworkRequest(url: "https://postman-echo.com/get").responseVoidV3().metadata
+    func testResponseVoid() async throws {
+        let metadata = try await NetworkRequest(url: "https://postman-echo.com/get").responseVoid().metadata
         XCTAssertNotNil(metadata)
     }
 
-    func testResponseJSONV3() async throws {
-        let response = try await NetworkRequest(url: "https://postman-echo.com/get").responseJSONV3()
+    func testResponseJSON() async throws {
+        let response = try await NetworkRequest(url: "https://postman-echo.com/get").responseJSON()
         let json = response.json
         XCTAssertNotNil(json)
         XCTAssertNotNil(response.metadata)
     }
 
-    func testResponseDecodableV3() async throws {
+    func testResponseDecodable() async throws {
         let url = "https://postman-echo.com/get?foo1=bar1&foo2=bar2"
-        let response = try await NetworkRequest(url: url).responseDecodableV3() as NetworkResponseDecodable<PostmanGetResponseCodable>
+        let response = try await NetworkRequest(url: url).responseDecodable() as NetworkResponseDecodable<PostmanGetResponseCodable>
         let postmanGetResponseModel = response.decodableObject
         XCTAssertEqual(postmanGetResponseModel.args, ["foo1": "bar1", "foo2": "bar2"])
         XCTAssertNotNil(response.metadata)
+    }
+
+    func testFailureCase() async throws {
+        
+    }
+
+    /// In this test we will only consider the request successful if the status code is 500. The test API will return 200, so the request should complete with an error.
+    func testStatusCodeValidation() async throws {
+        let url = "https://postman-echo.com/get"
+        let validStatusCodes = [500]
+
+        do {
+            let _ = try await NetworkRequest(url: url, validStatusCodes: validStatusCodes).responseData()
+            XCTFail("Request should have failed and execution should have jumped to catch block")
+        } catch {
+            guard let error = error as? NetworkResponseError else {
+                XCTFail()
+                return
+            }
+            XCTAssertNotEqual(error.responseMetadata?.response?.statusCode, 500)
+            switch error.error as? NetworkError {
+            case .some(.invalidStatusCode):
+                break
+            default:
+                XCTFail("Error should have been .invalidStatusCode")
+            }
+        }
     }
 }
